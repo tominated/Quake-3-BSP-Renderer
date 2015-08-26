232fab840d0e756b9895df9bfe8a87574461f309
Tom Brunoli <tom@blackmilkclothing.com>
2015-08-26 16:47:57 +1000

    Initial Commit

---
+++ Quake 3 BSP Renderer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quake 3 BSP Renderer
+//
+//  Created by Thomas Brunoli on 26/08/2015.
+//  Copyright (c) 2015 Thomas Brunoli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
